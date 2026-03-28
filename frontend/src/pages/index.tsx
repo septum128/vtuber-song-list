bc160204a65b932a47d85257ba114b2c15c22f0e
septum128 <97213355+septum128@users.noreply.github.com>
2026-03-29 05:40:26 +0900

    v1.09 Topページに注意事項、お礼、問合せ先、不具合報告先を追加

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -9,6 +9,37 @@ export default function Home() {
       <p className="text-body-secondary">
         VTuberの歌枠セトリデータベースへようこそ。
       </p>
+      <p className="text-danger">
+        このサイトはファンが非公式に運営しているものです。
+        <br />
+        このサイトに関して配信者様本人へ問い合わせをされるのはお控えください。
+      </p>
+      <p>
+        セトリはYouTubeのコメント欄を元に作成しています。いつもセトリを作ってくださっている方々に感謝を。ありがとうございます！
+      </p>
+      <p>
+        歌枠データベースは裏側で動くプログラムによって自動更新されているため、誤った情報が登録されていることがあります。
+      </p>
+      <p>
+        ユーザ登録（ID/パスワード認証）をしていただくと、セトリの修正提案が可能ですので、そちらからセトリの修正申請をお願いいたします。
+      </p>
+      <p>
+        お問い合わせは
+        <a href="https://twitter.com/interceptor128" target="_blank">
+          Twitter
+        </a>
+        か<a href="mailto:contact@vtuber-song.com">メール</a>へ！
+      </p>
+      <p>
+        不具合報告はGitHubの
+        <a
+          href="https://github.com/septum128/vtuber-song-list/issues"
+          target="_blank"
+        >
+          Issue
+        </a>
+        へ投稿をお願いいたします。
+      </p>
     </DefaultLayout>
   );
 }
